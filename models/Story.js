e5f72c9df9ba4c0fb07e921e244774a0a847779d
Brian <galyenb@vcu.edu>
2023-11-29 19:06:12 -0500

    added story and userStory models/associations --- TODO: Review Associations

---
+++ models/Story.js
@@ -0,0 +1,33 @@
+//Used for the main blog post
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Story extends Model {}
+
+Story.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'story',
+  }
+);
+
+module.exports = Story;
