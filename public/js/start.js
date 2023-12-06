02ecf4fb8f41e768d8072a13d7fd61cce13ed6c3
christiecamp <christiecamphoto@gmail.com>
2023-12-06 16:17:18 -0500

    minor changes

--- public/js/start.js
+++ public/js/start.js
@@ -19,7 +19,7 @@ const startNewAdventureHandler = async (event) => {
         function getMaxId(elements) {
           return Math.max(
             ...elements
-              .map((el) => el.id)
+              .map((elements) => elements.id)
               .filter((id) => !isNaN(Number(id)))
               .map(Number)
           );
@@ -32,10 +32,10 @@ const startNewAdventureHandler = async (event) => {
         if (!allElementsInDiv.length) {
           document.location.replace('/storyline/1');
         } else {
-          const maxId = getMaxId(allElementsInDiv);
+          let maxId = getMaxId(allElementsInDiv);
           console.log(maxId);
           if (maxId === 0) {
-            document.location.replace(`/storyline/${maxId + 1}`);
+            document.location.replace(`/storyline/${maxId ++ }`);
           } else if (maxId !== -Infinity) {
             // document.location.replace(`/stosryline/${maxId}`);
           } else {
