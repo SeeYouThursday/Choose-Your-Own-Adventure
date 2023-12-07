ea58c72d5153189875f0222d07680e8640491f0a
Brian <galyenb@vcu.edu>
2023-12-06 21:47:22 -0500

    fixing merge issue

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ defaultLayout: 'main' });
 
 const sess = {
-  secret: 'luck secret',
+  secret: process.env.SECRET,
   cookie: {
     maxAge: 20000000,
     httpOnly: true,
